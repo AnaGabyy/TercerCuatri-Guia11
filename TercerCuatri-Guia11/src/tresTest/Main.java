10e550e383b8a6405639628dc91f43e4af006a8b
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:30:09 -0300

    Clase Main ej3
    Enunciado

---
+++ TercerCuatri-Guia11/src/tresTest/Main.java
@@ -0,0 +1,20 @@
+package tresTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Se desea hacer un sistema para un supermercado.
+        En un supermercado se venden diferentes tipos de productos, como alimentos, bebidas, productos de limpieza,
+        entre otros. Los productos se pueden clasificar en dos categorías: perecederos y no perecederos. Los productos
+        perecederos tienen una fecha de caducidad y los productos no perecederos no tienen fecha de caducidad.
+        Se desea implementar un sistema que permita registrar las ventas realizadas en el supermercado. Además de
+        contar con el inventario de productos vendidos y en stock. La venta tiene que estar marcada con un id de venta
+        y una fecha de cuando se realizó la venta
+
+        –OPCIONAL.
+        También se desea incorporar un sistema de membresía donde haya descuentos en base a los productos si la venta
+        es o no es por membresía. Ir actualizando el stock a medida que se vaya vendiendo.
+*/
+
+    }
+}

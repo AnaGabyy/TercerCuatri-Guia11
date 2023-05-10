3d4b2d8a03869a5869a9cdb691797d25bd010ddf
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:30:20 -0300

    Clase Main ej2
    Enunciado

---
+++ TercerCuatri-Guia11/src/dosTest/Main.java
@@ -0,0 +1,24 @@
+package dosTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Se desea hacer el sistema para una Librería. La librería ofrece libros y revistas para la venta.
+        Cada libro tiene un título, un autor, un año de publicación y un precio. Cada revista tiene un título,
+        una fecha de publicación y un precio. Para representar los libros y las revistas, respectivamente.
+        Además, la librería ofrece un sistema de membresía que permite a los clientes obtener descuentos en sus compras.
+        Los clientes pueden ser miembros regulares o miembros VIP. Los miembros regulares tienen un descuento del 5%,
+        mientras que los miembros VIP tienen un descuento del 10%. Para representar los diferentes tipos de membresía,
+        se ha definido la interfaz Membresía, que tiene un método abstracto calcularDescuento().Se desea que el
+        programa permite realizar ventas en la librería. Para representar una venta, se ha definido la clase Venta,
+        que tiene como atributo una lista de productos que van a ser vendidos por la librería y el tipo de membresía
+        del cliente. Las ventas se irán almacenando en una lista.
+
+        Por último, se ha definido la clase Librería, que tiene como atributo una lista de ventas realizadas.
+        Esta clase tiene métodos para agregar una venta a la lista, calcular el total a pagar por todas las ventas
+        realizadas y contar la cantidad de libros y revistas vendidos. El programa debe permitir al usuario agregar
+        ventas a la librería, y luego mostrar el total a pagar y la cantidad de libros y revistas vendidos.
+*/
+
+    }
+}

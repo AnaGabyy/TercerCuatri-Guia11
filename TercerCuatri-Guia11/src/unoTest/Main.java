1f43673f55c65a107ea173a0ea6dcd166e2ca8cb
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:29:58 -0300

    Clase Main ej1
    Enunciado

---
+++ TercerCuatri-Guia11/src/unoTest/Main.java
@@ -0,0 +1,23 @@
+package unoTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Se desea hacer un sistema para una Cafetería.
+
+        En la cafetería venden distintos tipos de bebidas tales como Café, Té, Chocolate. Estos pueden ser del tipo
+        frío o caliente.
+
+        Los precios son:
+
+        1000 pesos 1 cafe
+        700 pesos un té
+        1500 pesos una chocolatada
+
+        El sistema tiene que recibir pedidos / órdenes, donde un pedido/ orden consta de una o más bebidas y un
+        total por eso. Se desea almacenar los pedidos entrantes y salientes, así como también informar al final
+        del día todos los pedidos ingresados, total por pedido y total acumulado de pedidos. También cobrar un
+        adicional de 100 pesos si es caliente o 200 si es fría la bebida.*/
+
+    }
+}
